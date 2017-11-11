2af0d58e77aae9adf89360909ed004c5ae2d9b68
Nathaniel Stiers <nvstiers@gmail.com>
2017-11-11 18:27:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,19 +14,15 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 
-function sayHiToGrandma (string) {
-  if (string.toEqual('hello')); 
-  return "I can't hear you!";
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!";
+  }
+
+  else if (string.toUpperCase() === string) {
+    return 'YES INDEED';
+  }
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
-// function sayHiToGrandma(string) {
-//   if (string.toEqual('hello')) {
-//     return "I can't hear you!";
-//   }
-//
-//   else if (string.toEqual('HELLO')) {
-//     return 'YES INDEED';
-//   }
-//   else if (string.toEqual("I love you, Grandma.")) {
-//     return "I love you, too."
-//   }
-// }
