bb83187914acd3d39c2bb8b5d1d4309de7cd2680
Nathaniel Stiers <nvstiers@gmail.com>
2017-11-11 18:23:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma (string) {
-  string.toEqual('hello'); 
+  if (string.toEqual('hello')); 
   return "I can't hear you!";
 }
 // function sayHiToGrandma(string) {
